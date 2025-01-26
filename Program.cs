cff55a571069dd3ba20ee92b884840b8dd0a8ce0
samnolasco <93192083+samnolasco@users.noreply.github.com>
2025-01-26 23:24:42 +0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿namespace DATASTRUCTPR1;
+namespace DATASTRUCTPR1;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using Amazon.Runtime;
 using cmd;
@@ -25,129 +26,132 @@ internal class Program
         //List<info> dinein = infos.Where(x => x.DineorTake == "Dine-in").ToList();
         //Console.WriteLine($"The number of dine in customers are: {infos.Count()}");
 
-        //foreach (info info in infos) 
+        //foreach (info info in infos)
         //{
         //    Console.WriteLine($"Name: {info.CustomerName} - Customer Number: {info.CusNum}");
+        //}
 
 
-        //ORDER BY
-        List<info> orderedCustomers = infos.OrderBy(x => x.CustomerName).OrderBy(x => x.CusNum).ToList();
-        List<info> dineincustomers = infos.Where(x => x.DineorTake == "Dine-In").ToList();
-        Console.WriteLine($"The number of dine in customers is {dineincustomers.Count}");
-        foreach (info info in orderedCustomers)
+
+
+
+        ////ORDER BY
+        List<info> orderedCustomers = infos.OrderBy(x => x.CusNum).OrderBy(x => x.DineorTake).ToList();
+        foreach (var info in orderedCustomers)
         {
-            Console.WriteLine($"Name: {info.CustomerName} {info.CusNum} - {info.DineorTake}");
+            Console.WriteLine($"ORDER BY\n Customer Name: {info.CustomerName}\n Customer Number: {info.CusNum}\n - {info.DineorTake}\n\n");
         }
+
 
         //FILTER
-        List<info> dineincustomerss = infos.Where(x => x.DineorTake == "Take-out").ToList() ;   
-        Console.WriteLine($"The number of take-out customers is {infos.Count}");      
-        foreach(info info in dineincustomers)
+        List<info> dineincustomerss = infos.Where(x => x.DineorTake == "Take-out").OrderBy(x => x.DineorTake).ToList();
+
+        Console.WriteLine($"The number of take-out customers is {dineincustomerss.Count}");
+        foreach (var info in dineincustomerss)
         {
-            Console.WriteLine($"Name: {info.CustomerName} {info.DineorTake}-{info.CusNum}");
+            Console.WriteLine($"Customer Name: {info.CustomerName}\n{info.DineorTake}\nCustomer Number: {info.CusNum}\n\n");
         }
-        
+
+
         //GROUP
         var grouped = infos.GroupBy(x => x.DineorTake);
-        foreach (var item in grouped)
+        foreach (var info in grouped)
         {
-            Console.WriteLine ($"DINEIN: {item.Key}");  
+            Console.WriteLine ($"CHOICES: {info.Key}");  
         }
-                   
 
 
 
-
-            //LinQ - capability to manipulate 
-
-
-            //  //instantiate
-
-            // // menu espressobased = new menu(); //instantiation
-            ////  espressobased.ProdName = "Mocha Breve"; //dot or period access operator
-            //  espressobased.Price = 139;
-            //  espressobased.IsBestSeller = true;
-
-            //  menu noncoffee = new menu(); //instantiation
-            //  noncoffee.ProdName = "Matcha Latte";
-            //  noncoffee.Price = 149;
-            //  noncoffee.IsBestSeller = true;
+        //LinQ - capability to manipulate 
 
 
-            //  menu frappuccino = new menu(); //instantiation
-            //  frappuccino.ProdName = "Oreo Cheesecake";
-            //  frappuccino.Price = 149;
-            //  frappuccino.IsBestSeller = true;
+
+        // // menu espressobased = new menu(); //instantiation
+        ////  espressobased.ProdName = "Mocha Breve"; //dot or period access operator
+        //  espressobased.Price = 139;
+        //  espressobased.IsBestSeller = true;
+
+        //  menu noncoffee = new menu(); //instantiation
+        //  noncoffee.ProdName = "Matcha Latte";
+        //  noncoffee.Price = 149;
+        //  noncoffee.IsBestSeller = true;
 
 
-            //  menu croffles = new menu(); //instantiation
-            //  croffles.ProdName = "Biscoff Croffles";
-            //  croffles.Price = 119;
-            //  croffles.IsBestSeller = false;
+        //  menu frappuccino = new menu(); //instantiation
+        //  frappuccino.ProdName = "Oreo Cheesecake";
+        //  frappuccino.Price = 149;
+        //  frappuccino.IsBestSeller = true;
 
 
-            //  List<menu> menu = new List<menu>();
-            //  menu.Add(espressobased);
-            //  menu.Add(noncoffee);
-            //  menu.Add(frappuccino);
-            //  menu.Add(croffles);
+        //  menu croffles = new menu(); //instantiation
+        //  croffles.ProdName = "Biscoff Croffles";
+        //  croffles.Price = 119;
+        //  croffles.IsBestSeller = false;
 
-            //  menu meow = new menu()
-            //  {
-            //      ProdName = "Carbonara",
-            //      Price = 145,
-            //      IsBestSeller = false,
-            //  };
 
-            //  menu.Add(meow);
+        //  List<menu> menu = new List<menu>();
+        //  menu.Add(espressobased);
+        //  menu.Add(noncoffee);
+        //  menu.Add(frappuccino);
+        //  menu.Add(croffles);
 
-            //  //instantation while adding in a list
-            //  menu.Add(new menu { ProdName = "Strawberry Oreo", Price = 145, IsBestSeller = true });
+        //  menu meow = new menu()
+        //  {
+        //      ProdName = "Carbonara",
+        //      Price = 145,
+        //      IsBestSeller = false,
+        //  };
 
-            //  //foreach (var subjectGrade in subjectGrades)
-            //  //{
-            //  //    Console.WriteLine($"Subject: {subjectGrade.SubjectCode}\tGrade: {subjectGrade.Grade}\tMajor:{subjectGrade.IsSubjectMajor}");
-            //  //}
+        //  menu.Add(meow);
 
-            //  info customer = new info()
-            //  {
-            //      CustomerName = "John Marc Gabuyo",
-            //      CusNum = "00012345",
-            //      DineorTake = "Dine-In",
-            //      ror = menu
-            //  };
+        //  //instantation while adding in a list
+        //  menu.Add(new menu { ProdName = "Strawberry Oreo", Price = 145, IsBestSeller = true });
 
-            //  List<info> SatiyaCust = new List<info>();
-            //  SatiyaCust.Add(customer);
+        //  //foreach (var subjectGrade in subjectGrades)
+        //  //{
+        //  //    Console.WriteLine($"Subject: {subjectGrade.SubjectCode}\tGrade: {subjectGrade.Grade}\tMajor:{subjectGrade.IsSubjectMajor}");
+        //  //}
 
-            //  List<menu> menudo = new List<menu>()
-            //  {
-            //      new menu {ProdName = "Mocha Breve", Price = 139, IsBestSeller = true},
-            //      new menu {ProdName = "Matcha Latte", Price = 149, IsBestSeller = true},
-            //      new menu {ProdName = "Oreo Cheesecake", Price = 149, IsBestSeller = true},
-            //      new menu {ProdName = "Biscoff Croffles", Price = 119, IsBestSeller = false},
-            //  };
+        //  info customer = new info()
+        //  {
+        //      CustomerName = "John Marc Gabuyo",
+        //      CusNum = "00012345",
+        //      DineorTake = "Dine-In",
+        //      ror = menu
+        //  };
 
-            //  SatiyaCust.Add(new info
-            //  {
-            //      CustomerName = "Keiann Kelly Gonzales",
-            //      CusNum = "00012346",
-            //      DineorTake = "Take out",
-            //      ror = menudo
-            //  });
+        //  List<info> SatiyaCust = new List<info>();
+        //  SatiyaCust.Add(customer);
 
-            //  Console.WriteLine("<< SATIYA CAFE >>\n");
+        //  List<menu> menudo = new List<menu>()
+        //  {
+        //      new menu {ProdName = "Mocha Breve", Price = 139, IsBestSeller = true},
+        //      new menu {ProdName = "Matcha Latte", Price = 149, IsBestSeller = true},
+        //      new menu {ProdName = "Oreo Cheesecake", Price = 149, IsBestSeller = true},
+        //      new menu {ProdName = "Biscoff Croffles", Price = 119, IsBestSeller = false},
+        //  };
 
-            //  foreach (var customers in SatiyaCust)
-            //  {
-            //      Console.WriteLine($"\nCUSTOMER NAME: \t{customers.CustomerName}");
-            //      Console.WriteLine($"Customer Number: \t{customers.CusNum} ");
-            //      Console.WriteLine($"DINE-IN OR TAKEOUT: \t{customers.DineorTake} ");
-            //      Console.WriteLine("\n....MENU....");
+        //  SatiyaCust.Add(new info
+        //  {
+        //      CustomerName = "Keiann Kelly Gonzales",
+        //      CusNum = "00012346",
+        //      DineorTake = "Take out",
+        //      ror = menudo
+        //  });
 
-            //      foreach (var menuu in customers.ror)
-            //      {
-            //          Console.WriteLine($"Product Name: {menuu.ProdName}\tPrice: {menuu.Price}\tisBestSeller?:{menuu.IsBestSeller}");
-            //      }
+        //  Console.WriteLine("<< SATIYA CAFE >>\n");
+
+        //  foreach (var customers in SatiyaCust)
+        //  {
+        //      Console.WriteLine($"\nCUSTOMER NAME: \t{customers.CustomerName}");
+        //      Console.WriteLine($"Customer Number: \t{customers.CusNum} ");
+        //      Console.WriteLine($"DINE-IN OR TAKEOUT: \t{customers.DineorTake} ");
+        //      Console.WriteLine("\n....MENU....");
+
+        //      foreach (var menuu in customers.ror)
+        //      {
+        //          Console.WriteLine($"Product Name: {menuu.ProdName}\tPrice: {menuu.Price}\tisBestSeller?:{menuu.IsBestSeller}");
+        //      }
     }
 }
+
